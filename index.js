cbc869760c3385f11f40724786d62274311f47ce
VsoHumansa <humansa@vsoloop.com>
2022-08-22 18:28:57 +0800

    [update]

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { Spring, animated, useSpring } from "@react-spring/native";
+
 
 const GameTest = (props) => {
-    return <Text>AAA KKK {props.label}</Text>;
+    const { opacity, rotateX } = useSpring({
+        opacity: 1,
+        rotateX: "180deg",
+        from: { opacity: 0, rotateX: "0deg" },
+        config: { mass: 5, tension: 500, friction: 80 }
+    });
+
+    return <AnimatedView style={{opacity, transform: [{rotateX}]}}>
+        <Text>AAA KKK {props.label}</Text>
+    </AnimatedView>;
 };
 
 export default GameTest;
