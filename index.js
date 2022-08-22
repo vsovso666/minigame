9a472606b9891a95ab9f03c46154e11dcdf124d1
VsoHumansa <humansa@vsoloop.com>
2022-08-22 18:00:25 +0800

    [update] test content

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const GameTest = (string) => {
+    <View>
+        <Text>"i am game test" + string</Text>
+    </View>
+}
+
+export default GameTest;
