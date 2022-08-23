c3223b7d8045a80228f69e0b517163c9a0927d11
VsoHumansa <humansa@vsoloop.com>
2022-08-23 18:10:44 +0800

    [update] minigame development

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Spring, animated, useSpring } from "@react-spring/native";
+import React, { useState, useEffect } from 'react';
 
-const AnimatedView = animated(View);
+import {GameEngine} from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
+
+import {StyleSheet, Text, Button, View, TouchableOpacity, Alert} from 'react-native';
 
 const GameTest = (props) => {
-    const { opacity, rotateX } = useSpring({
-        opacity: 1,
-        rotateX: "180deg",
-        from: { opacity: 0, rotateX: "0deg" },
-        config: { mass: 5, tension: 500, friction: 80 }
-    });
+    const [running, setRunning] = useState(false);
+    const [gameEngine, setGameEngine] = useState(null);
+    const [currentPoints, setCurrentPoints] = useState(0);
 
-    return <AnimatedView style={{opacity, transform: [{rotateX}]}}>
-        <Text>AAA KKK {props.label}</Text>
-    </AnimatedView>;
+    useEffect(() => {
+        setRunning(true)
+    }, []);
+
+    const actionEndGame = () => {
+        gameEngine && gameEngine.stop();
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20 }}>{currentPoints}</Text>
+            <GameEngine
+            ref={(ref) => { setGameEngine(ref) }}
+            style={styles.engineWrapper}
+            systems={[Physics]}
+            entities={entities()}
+            running={running}
+            onEvent={(e) => {
+                switch (e.type) {
+                case 'game_over':
+                    setRunning(false)
+                    gameEngine.stop()
+                    break;
+                case 'new_point':
+                    setCurrentPoints(currentPoints + 1)
+                    break;
+                }
+            }}
+            ></GameEngine>
+
+            {!running ?
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <TouchableOpacity style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
+                onPress={() => {
+                    setCurrentPoints(0)
+                    setRunning(true)
+                    gameEngine.swap(entities())
+                }}>
+                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+                    START GAME
+                </Text>
+                </TouchableOpacity>
+
+            </View> : null}
+
+            <View style={styles.btn}>
+            <Button title="stop"onPress={() => actionEndGame()}></Button>
+            </View>
+
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#DDDDDD',
+    },
+    engineWrapper: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    btn: {
+        position: 'absolute',
+        bottom: 10,
+        left: 10,
+        backgroundColor: 'red'
+    }
+});
 
 export default GameTest;
